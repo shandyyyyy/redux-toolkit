ef00e37ebe514357e7f9aab8227f8accbf6454bc
shandy <16640436+shandyyyyy@users.noreply.github.com>
2023-08-09 15:47:41 +0800

    Delete thunk.ts

--- thunk.ts
+++
@@ -1,8 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
-
-
-export const getUserThunk = createAsyncThunk('users/getUserAction', async () => {
-  const response = await axios.get(`your-server-url:port/api/users`);
-  return response.data
-})
